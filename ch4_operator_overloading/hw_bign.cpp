2cf8e02d6c178c333c833cbb87bbc3410ccd65df
张筱涵 <1723557950@qq.com>
2026-05-06 14:51:35 +0800

    文件整理

---
+++ ch4_operator_overloading/hw_bign.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+
+bign a, b, c;
+
+a = "123456789123456789987654321999999999999999999999999";
+
+cin >> b;
+
+c = 12345;
+
+cout << (a + b) << endl;
+
+cout << (a - b) << endl;
+
+cout << (a * b) << endl;
+
+cout << (a / b) << endl;
+
+cout << (a < b) << endl;
+
+cout << (a > c) << endl;
+
+a += b;
+
+cout << a << endl;
+
+cout << (a <= b) << endl;
+
+cout << (a >= c) << endl;
+
+a += c;
+
+cout << a << endl;
+
+cout << (a == b) << endl;
+
+cout << (a != c) << endl;
+
+return 0;
+
+}
+
+
---
+++ ch5_template_and_Generic_programming/Template/hw6_myqueue.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+template <typename T> class MyQueue;//前置声明
+template <typename T> ostream & operator<<(ostream &, const MyQueue<T> &); 
+
+template <typename T> 
+class QueueItem
+{
+public:
+    QueueItem(const T & t) :item(t), next(0)
+    {} 
+private:
+    T item; 
+    QueueItem *next; 
+    friend class MyQueue<T>;//友元类    
+    //通过友元函数重载<<运算符模板函数，要写上<<后的<Type>
+    friend ostream & operator<< <T> (ostream & os, const MyQueue<T> & q);
+};
+template <typename T>
+ostream & operator<< <T> (ostream & os, const MyQueue<T> & q){
+    os<<
+    return os;
+}
+template <typename T> 
+class MyQueue
+{
+public:
+    MyQueue() : head(0), tail(0) {} // Empty MyQueue
+
+    MyQueue(const MyQueue &Q) //拷贝构造函数
+        :head(0), tail(0) 
+    { CopyElements(Q); };
+
+    ~MyQueue() { Destroy(); }
+
+    MyQueue & operator=(const MyQueue &);
+
+    // return element from head of MyQueue
+    T & Front() { return head->item; }
+    const T & Front() const { return head->item; }
+    void Push(const T &); //add element to back of MyQueue
+    void Pop(); // remove element from head of MyQueue
+    bool Empty() const { return head == 0; }
+    void Display() const;
+private:
+    QueueItem<T> *head;
+    QueueItem<T> *tail;
+    void Destroy(); //delete all the elements
+    void CopyElements(const MyQueue &);
+
+    //设置友元函数
+    friend ostream & operator<< <T> (ostream & os, const MyQueue<T> & q);
+};
+
+int main()
+{
+    MyQueue<int> qi;
+    qi.Push(1);
+    qi.Push(2);
+    qi.Push(3);
+    qi.Push(4);
+    qi.Push(5);
+    qi.Pop();
+    qi.Display();
+    cout<<"\n";
+    cout<<qi;
+    cout<<endl;
+
+    MyQueue<int> qi2(qi);
+    qi2.Display();
+    cout<<endl;
+
+    MyQueue<int> qi3;
+    qi3 = qi;
+    cout<<qi3;
+
+    return 0;
+}
